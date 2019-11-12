bbee08e61d337ecbd6563c3231ccff3afe5cd216
Lars Kiesow <lkiesow@uos.de>
2019-11-12 02:15:43 +0100

    remotes/upstream/convert-to-themeui

---
+++ precache-manifest.fecffce5580fb40ea963fd56ee652930.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6a9ae2fa123dc4c8c26d779644aca522",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ef9ae9225b6cea47d6fc",
+    "url": "/static/js/2.873c55b9.chunk.js"
+  },
+  {
+    "revision": "ddb778d7918ff3bb7fb9",
+    "url": "/static/js/3.054d3c3c.chunk.js"
+  },
+  {
+    "revision": "f53c63223c48e473c89c",
+    "url": "/static/js/4.01273466.chunk.js"
+  },
+  {
+    "revision": "72a76fad3045778edb70",
+    "url": "/static/js/main.b7cbd07c.chunk.js"
+  },
+  {
+    "revision": "2f4df881304fa7974a7e",
+    "url": "/static/js/runtime-main.ead14e2b.js"
+  },
+  {
+    "revision": "2f67c1960160f2d005a13ac5e21b9e56",
+    "url": "/static/media/iran.2f67c196.png"
+  },
+  {
+    "revision": "6f382b2ddfdd4ea349ac9ca989da123a",
+    "url": "/static/media/united-kingdom.6f382b2d.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/build-388/precache-manifest.f0ae07b43bd3bc9c0215d33e926296b7.js"
+  "/precache-manifest.fecffce5580fb40ea963fd56ee652930.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/build-388/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
