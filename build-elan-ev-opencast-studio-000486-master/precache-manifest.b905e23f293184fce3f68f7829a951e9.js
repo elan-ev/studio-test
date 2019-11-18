fa2ce734faff8e62f503b42ed3b2f6048e6575af
Travis CI User <travis@example.org>
2019-11-18 21:27:24 +0000

    Build #486 (Mon Nov 18 21:27:24 UTC 2019) | c26927a Merge pull request #177 from elan-ev/fix-issue-176

---
+++ build-elan-ev-opencast-studio-000486-master/precache-manifest.b905e23f293184fce3f68f7829a951e9.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bec397734f26a084b12eaccc1b2da809",
+    "url": "/build-elan-ev-opencast-studio-000486-master/index.html"
+  },
+  {
+    "revision": "ce7273619919ad01a5f7",
+    "url": "/build-elan-ev-opencast-studio-000486-master/static/js/2.9f488733.chunk.js"
+  },
+  {
+    "revision": "a75501348353fec2f166",
+    "url": "/build-elan-ev-opencast-studio-000486-master/static/js/3.66f0e4ca.chunk.js"
+  },
+  {
+    "revision": "9286dbb09c5a7f3c5faf",
+    "url": "/build-elan-ev-opencast-studio-000486-master/static/js/4.c9f47efb.chunk.js"
+  },
+  {
+    "revision": "6406b6e5c211dd1c9148",
+    "url": "/build-elan-ev-opencast-studio-000486-master/static/js/main.69ee9631.chunk.js"
+  },
+  {
+    "revision": "fc6f08044b223aebef6b",
+    "url": "/build-elan-ev-opencast-studio-000486-master/static/js/runtime-main.8e664a29.js"
+  },
+  {
+    "revision": "2f67c1960160f2d005a13ac5e21b9e56",
+    "url": "/build-elan-ev-opencast-studio-000486-master/static/media/iran.2f67c196.png"
+  },
+  {
+    "revision": "6f382b2ddfdd4ea349ac9ca989da123a",
+    "url": "/build-elan-ev-opencast-studio-000486-master/static/media/united-kingdom.6f382b2d.png"
+  }
+]);
