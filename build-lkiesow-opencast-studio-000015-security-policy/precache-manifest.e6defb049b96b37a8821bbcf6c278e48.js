f458fd4648d8f660707e0b9ba0f316d136f63000
Travis CI User <travis@example.org>
2019-11-18 22:45:45 +0000

    Build #15 (Mon Nov 18 22:45:45 UTC 2019) | f850dad Add Security Policy

---
+++ build-lkiesow-opencast-studio-000015-security-policy/precache-manifest.e6defb049b96b37a8821bbcf6c278e48.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0ae3cf929362bb40741dc059451b6011",
+    "url": "/build-lkiesow-opencast-studio-000015-security-policy/index.html"
+  },
+  {
+    "revision": "ce7273619919ad01a5f7",
+    "url": "/build-lkiesow-opencast-studio-000015-security-policy/static/js/2.9f488733.chunk.js"
+  },
+  {
+    "revision": "a75501348353fec2f166",
+    "url": "/build-lkiesow-opencast-studio-000015-security-policy/static/js/3.66f0e4ca.chunk.js"
+  },
+  {
+    "revision": "a6e88da73357f2cebca6",
+    "url": "/build-lkiesow-opencast-studio-000015-security-policy/static/js/4.a3a4c423.chunk.js"
+  },
+  {
+    "revision": "f9703ca5940fe76d17ed",
+    "url": "/build-lkiesow-opencast-studio-000015-security-policy/static/js/main.fba0fe03.chunk.js"
+  },
+  {
+    "revision": "2a63e32253399a4b6abc",
+    "url": "/build-lkiesow-opencast-studio-000015-security-policy/static/js/runtime-main.4f666ba2.js"
+  },
+  {
+    "revision": "2f67c1960160f2d005a13ac5e21b9e56",
+    "url": "/build-lkiesow-opencast-studio-000015-security-policy/static/media/iran.2f67c196.png"
+  },
+  {
+    "revision": "6f382b2ddfdd4ea349ac9ca989da123a",
+    "url": "/build-lkiesow-opencast-studio-000015-security-policy/static/media/united-kingdom.6f382b2d.png"
+  }
+]);
