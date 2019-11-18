da78a7f3f9425672f83d0aa2ef2c22d54dc77b11
Travis CI User <travis@example.org>
2019-11-18 16:04:32 +0000

    Build #481 (Mon Nov 18 16:04:32 UTC 2019) | d609c4a Introduce Test Deployments

---
+++ build-elan-ev-opencast-studio-000481-test-deployment/precache-manifest.4afe8c7850df9a9250fd93a765c34f66.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4f9a7b5bf3e93667f29c84432f9b8d88",
+    "url": "/build-elan-ev-opencast-studio-000481-test-deployment/index.html"
+  },
+  {
+    "revision": "ce7273619919ad01a5f7",
+    "url": "/build-elan-ev-opencast-studio-000481-test-deployment/static/js/2.9f488733.chunk.js"
+  },
+  {
+    "revision": "a75501348353fec2f166",
+    "url": "/build-elan-ev-opencast-studio-000481-test-deployment/static/js/3.66f0e4ca.chunk.js"
+  },
+  {
+    "revision": "dfe4b6203195d0e8139a",
+    "url": "/build-elan-ev-opencast-studio-000481-test-deployment/static/js/4.dd798b5e.chunk.js"
+  },
+  {
+    "revision": "4da4a5ef97f6f5eb2a13",
+    "url": "/build-elan-ev-opencast-studio-000481-test-deployment/static/js/main.84b7fb2f.chunk.js"
+  },
+  {
+    "revision": "084216346e70b5834c7b",
+    "url": "/build-elan-ev-opencast-studio-000481-test-deployment/static/js/runtime-main.77785d10.js"
+  },
+  {
+    "revision": "2f67c1960160f2d005a13ac5e21b9e56",
+    "url": "/build-elan-ev-opencast-studio-000481-test-deployment/static/media/iran.2f67c196.png"
+  },
+  {
+    "revision": "6f382b2ddfdd4ea349ac9ca989da123a",
+    "url": "/build-elan-ev-opencast-studio-000481-test-deployment/static/media/united-kingdom.6f382b2d.png"
+  }
+]);
