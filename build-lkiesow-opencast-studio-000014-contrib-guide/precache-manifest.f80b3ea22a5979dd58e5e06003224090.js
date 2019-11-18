5fdeaabb3c71b4cf3a0cb5868cb3239bffba7ee7
Travis CI User <travis@example.org>
2019-11-18 22:18:07 +0000

    Build #14 (Mon Nov 18 22:18:07 UTC 2019) | 6da9406 Add Contribution Guide

---
+++ build-lkiesow-opencast-studio-000014-contrib-guide/precache-manifest.f80b3ea22a5979dd58e5e06003224090.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0f2cff5beb0cc2fc0bd4c236a2d664c4",
+    "url": "/build-lkiesow-opencast-studio-000014-contrib-guide/index.html"
+  },
+  {
+    "revision": "ce7273619919ad01a5f7",
+    "url": "/build-lkiesow-opencast-studio-000014-contrib-guide/static/js/2.9f488733.chunk.js"
+  },
+  {
+    "revision": "a75501348353fec2f166",
+    "url": "/build-lkiesow-opencast-studio-000014-contrib-guide/static/js/3.66f0e4ca.chunk.js"
+  },
+  {
+    "revision": "9d317be30258567cc3eb",
+    "url": "/build-lkiesow-opencast-studio-000014-contrib-guide/static/js/4.2dcbbaef.chunk.js"
+  },
+  {
+    "revision": "9bc2b4f4c5d30ccb6d14",
+    "url": "/build-lkiesow-opencast-studio-000014-contrib-guide/static/js/main.8f0a8f68.chunk.js"
+  },
+  {
+    "revision": "852a0575bd19220fd0da",
+    "url": "/build-lkiesow-opencast-studio-000014-contrib-guide/static/js/runtime-main.5dd7bee9.js"
+  },
+  {
+    "revision": "2f67c1960160f2d005a13ac5e21b9e56",
+    "url": "/build-lkiesow-opencast-studio-000014-contrib-guide/static/media/iran.2f67c196.png"
+  },
+  {
+    "revision": "6f382b2ddfdd4ea349ac9ca989da123a",
+    "url": "/build-lkiesow-opencast-studio-000014-contrib-guide/static/media/united-kingdom.6f382b2d.png"
+  }
+]);
