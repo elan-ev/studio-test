64de705dce597e1546d77b420b52867984623616
Travis CI User <travis@example.org>
2019-11-18 12:54:28 +0000

    Build #459 (Mon Nov 18 12:54:28 UTC 2019) | e0ace21 include slug

---
+++ build-elan-ev-opencast-studio-test-deployment-459/precache-manifest.3c9b35b7e020d74c0dcb69aac28d7e58.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3994bea4d12e7ff8f6cbcf5040042e5b",
+    "url": "/build-elan-ev-opencast-studio-test-deployment-459/index.html"
+  },
+  {
+    "revision": "e979ac982281689efa5b",
+    "url": "/build-elan-ev-opencast-studio-test-deployment-459/static/js/2.9a44bffa.chunk.js"
+  },
+  {
+    "revision": "3cb03440f7cb98edc712",
+    "url": "/build-elan-ev-opencast-studio-test-deployment-459/static/js/3.fb3f2bb4.chunk.js"
+  },
+  {
+    "revision": "68c7fc71d6152881daf0",
+    "url": "/build-elan-ev-opencast-studio-test-deployment-459/static/js/4.9976266b.chunk.js"
+  },
+  {
+    "revision": "f0c3ffabc22644a6c628",
+    "url": "/build-elan-ev-opencast-studio-test-deployment-459/static/js/main.041cd83d.chunk.js"
+  },
+  {
+    "revision": "8c4b261a73a5cc053527",
+    "url": "/build-elan-ev-opencast-studio-test-deployment-459/static/js/runtime-main.7ccf5246.js"
+  },
+  {
+    "revision": "2f67c1960160f2d005a13ac5e21b9e56",
+    "url": "/build-elan-ev-opencast-studio-test-deployment-459/static/media/iran.2f67c196.png"
+  },
+  {
+    "revision": "6f382b2ddfdd4ea349ac9ca989da123a",
+    "url": "/build-elan-ev-opencast-studio-test-deployment-459/static/media/united-kingdom.6f382b2d.png"
+  }
+]);
