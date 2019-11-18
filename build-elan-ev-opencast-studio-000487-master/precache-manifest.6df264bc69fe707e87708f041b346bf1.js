8854d53bf578c3f9dc6520183fd38c267a89ecef
Travis CI User <travis@example.org>
2019-11-18 21:45:34 +0000

    Build #487 (Mon Nov 18 21:45:34 UTC 2019) | 5e3d953 Add `pre-release-testing` document describing the QA testing process (#214)

---
+++ build-elan-ev-opencast-studio-000487-master/precache-manifest.6df264bc69fe707e87708f041b346bf1.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "350935b922f2ac661e0b004236adca75",
+    "url": "/build-elan-ev-opencast-studio-000487-master/index.html"
+  },
+  {
+    "revision": "ce7273619919ad01a5f7",
+    "url": "/build-elan-ev-opencast-studio-000487-master/static/js/2.9f488733.chunk.js"
+  },
+  {
+    "revision": "a75501348353fec2f166",
+    "url": "/build-elan-ev-opencast-studio-000487-master/static/js/3.66f0e4ca.chunk.js"
+  },
+  {
+    "revision": "824aab028a2dacfc09ad",
+    "url": "/build-elan-ev-opencast-studio-000487-master/static/js/4.faf5e11d.chunk.js"
+  },
+  {
+    "revision": "25f8f727304253753003",
+    "url": "/build-elan-ev-opencast-studio-000487-master/static/js/main.c45d6ea5.chunk.js"
+  },
+  {
+    "revision": "ba1b76a43620bfd09009",
+    "url": "/build-elan-ev-opencast-studio-000487-master/static/js/runtime-main.2c65ac30.js"
+  },
+  {
+    "revision": "2f67c1960160f2d005a13ac5e21b9e56",
+    "url": "/build-elan-ev-opencast-studio-000487-master/static/media/iran.2f67c196.png"
+  },
+  {
+    "revision": "6f382b2ddfdd4ea349ac9ca989da123a",
+    "url": "/build-elan-ev-opencast-studio-000487-master/static/media/united-kingdom.6f382b2d.png"
+  }
+]);
