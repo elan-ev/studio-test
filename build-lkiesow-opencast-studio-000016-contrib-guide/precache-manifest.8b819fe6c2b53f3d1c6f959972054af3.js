29bcc14fe8dc9095893ce7b8eccc2ac4c9b490ed
Travis CI User <travis@example.org>
2019-11-19 12:34:54 +0000

    Build #16 (Tue Nov 19 12:34:54 UTC 2019) | 9a904c6 Add Contribution Guide

---
+++ build-lkiesow-opencast-studio-000016-contrib-guide/precache-manifest.8b819fe6c2b53f3d1c6f959972054af3.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d362dc26f54d3b488265eefabc8a608a",
+    "url": "/build-lkiesow-opencast-studio-000016-contrib-guide/index.html"
+  },
+  {
+    "revision": "ce7273619919ad01a5f7",
+    "url": "/build-lkiesow-opencast-studio-000016-contrib-guide/static/js/2.9f488733.chunk.js"
+  },
+  {
+    "revision": "a75501348353fec2f166",
+    "url": "/build-lkiesow-opencast-studio-000016-contrib-guide/static/js/3.66f0e4ca.chunk.js"
+  },
+  {
+    "revision": "0f5948b66506ba76cf44",
+    "url": "/build-lkiesow-opencast-studio-000016-contrib-guide/static/js/4.dce919ef.chunk.js"
+  },
+  {
+    "revision": "3173142ac1ab0bb93f52",
+    "url": "/build-lkiesow-opencast-studio-000016-contrib-guide/static/js/main.871a6860.chunk.js"
+  },
+  {
+    "revision": "36200a5fb35df5f93f9c",
+    "url": "/build-lkiesow-opencast-studio-000016-contrib-guide/static/js/runtime-main.734614d8.js"
+  },
+  {
+    "revision": "2f67c1960160f2d005a13ac5e21b9e56",
+    "url": "/build-lkiesow-opencast-studio-000016-contrib-guide/static/media/iran.2f67c196.png"
+  },
+  {
+    "revision": "6f382b2ddfdd4ea349ac9ca989da123a",
+    "url": "/build-lkiesow-opencast-studio-000016-contrib-guide/static/media/united-kingdom.6f382b2d.png"
+  }
+]);
---
+++ build-lkiesow-opencast-studio-000016-contrib-guide/service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/build-lkiesow-opencast-studio-000016-contrib-guide/precache-manifest.8b819fe6c2b53f3d1c6f959972054af3.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/build-lkiesow-opencast-studio-000016-contrib-guide/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
