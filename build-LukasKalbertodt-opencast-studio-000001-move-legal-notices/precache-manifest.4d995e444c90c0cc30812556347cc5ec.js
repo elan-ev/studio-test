fb3dc0628b85bc8ea398b56c2684a4cd7a448a7b
Travis CI User <travis@example.org>
2019-11-19 15:29:53 +0000

    Build #1 (Tue Nov 19 15:29:52 UTC 2019) | 9110f07 Move "impressum" content to "about" page

---
+++ build-LukasKalbertodt-opencast-studio-000001-move-legal-notices/precache-manifest.4d995e444c90c0cc30812556347cc5ec.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a2c358a8584e9d81cf033e94de91919e",
+    "url": "/build-LukasKalbertodt-opencast-studio-000001-move-legal-notices/index.html"
+  },
+  {
+    "revision": "8f399b9f324ae944c9df",
+    "url": "/build-LukasKalbertodt-opencast-studio-000001-move-legal-notices/static/js/2.89b21343.chunk.js"
+  },
+  {
+    "revision": "7d278928686a990f6d7d",
+    "url": "/build-LukasKalbertodt-opencast-studio-000001-move-legal-notices/static/js/3.ab5eb70f.chunk.js"
+  },
+  {
+    "revision": "7b01a2809e9aaf477e4d",
+    "url": "/build-LukasKalbertodt-opencast-studio-000001-move-legal-notices/static/js/4.a677f24e.chunk.js"
+  },
+  {
+    "revision": "83ba83c47115382a171d",
+    "url": "/build-LukasKalbertodt-opencast-studio-000001-move-legal-notices/static/js/main.3abdd46d.chunk.js"
+  },
+  {
+    "revision": "1d53faa88d64a210761f",
+    "url": "/build-LukasKalbertodt-opencast-studio-000001-move-legal-notices/static/js/runtime-main.18b7d9ad.js"
+  },
+  {
+    "revision": "2f67c1960160f2d005a13ac5e21b9e56",
+    "url": "/build-LukasKalbertodt-opencast-studio-000001-move-legal-notices/static/media/iran.2f67c196.png"
+  },
+  {
+    "revision": "6f382b2ddfdd4ea349ac9ca989da123a",
+    "url": "/build-LukasKalbertodt-opencast-studio-000001-move-legal-notices/static/media/united-kingdom.6f382b2d.png"
+  }
+]);
