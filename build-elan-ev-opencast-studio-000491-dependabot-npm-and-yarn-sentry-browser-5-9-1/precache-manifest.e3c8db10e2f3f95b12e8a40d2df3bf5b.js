190acd48f108fde42de8f95b3f2162d75aaf78fe
Travis CI User <travis@example.org>
2019-11-19 05:00:54 +0000

    Build #491 (Tue Nov 19 05:00:54 UTC 2019) | d05baa0 build(deps): bump @sentry/browser from 5.9.0 to 5.9.1

---
+++ build-elan-ev-opencast-studio-000491-dependabot-npm-and-yarn-sentry-browser-5-9-1/precache-manifest.e3c8db10e2f3f95b12e8a40d2df3bf5b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "880446960d34c5b1025c744aa387e7da",
+    "url": "/build-elan-ev-opencast-studio-000491-dependabot-npm-and-yarn-sentry-browser-5-9-1/index.html"
+  },
+  {
+    "revision": "ce7273619919ad01a5f7",
+    "url": "/build-elan-ev-opencast-studio-000491-dependabot-npm-and-yarn-sentry-browser-5-9-1/static/js/2.9f488733.chunk.js"
+  },
+  {
+    "revision": "7d278928686a990f6d7d",
+    "url": "/build-elan-ev-opencast-studio-000491-dependabot-npm-and-yarn-sentry-browser-5-9-1/static/js/3.ab5eb70f.chunk.js"
+  },
+  {
+    "revision": "bee2f553bbd053dd4207",
+    "url": "/build-elan-ev-opencast-studio-000491-dependabot-npm-and-yarn-sentry-browser-5-9-1/static/js/4.dd4d6ce5.chunk.js"
+  },
+  {
+    "revision": "47dee72e87e7fe1fc2e0",
+    "url": "/build-elan-ev-opencast-studio-000491-dependabot-npm-and-yarn-sentry-browser-5-9-1/static/js/main.c015c6c2.chunk.js"
+  },
+  {
+    "revision": "da52f73d4ec30d10fd4d",
+    "url": "/build-elan-ev-opencast-studio-000491-dependabot-npm-and-yarn-sentry-browser-5-9-1/static/js/runtime-main.7c1f381f.js"
+  },
+  {
+    "revision": "2f67c1960160f2d005a13ac5e21b9e56",
+    "url": "/build-elan-ev-opencast-studio-000491-dependabot-npm-and-yarn-sentry-browser-5-9-1/static/media/iran.2f67c196.png"
+  },
+  {
+    "revision": "6f382b2ddfdd4ea349ac9ca989da123a",
+    "url": "/build-elan-ev-opencast-studio-000491-dependabot-npm-and-yarn-sentry-browser-5-9-1/static/media/united-kingdom.6f382b2d.png"
+  }
+]);
---
+++ build-elan-ev-opencast-studio-000491-dependabot-npm-and-yarn-sentry-browser-5-9-1/service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/build-elan-ev-opencast-studio-000491-dependabot-npm-and-yarn-sentry-browser-5-9-1/precache-manifest.e3c8db10e2f3f95b12e8a40d2df3bf5b.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/build-elan-ev-opencast-studio-000491-dependabot-npm-and-yarn-sentry-browser-5-9-1/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
