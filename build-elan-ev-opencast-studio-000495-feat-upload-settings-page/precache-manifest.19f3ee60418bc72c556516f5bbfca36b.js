5715614833bfdc085dd63fb5f8b9d26ce89cbb5e
Travis CI User <travis@example.org>
2019-11-19 08:47:19 +0000

    Build #495 (Tue Nov 19 08:47:19 UTC 2019) | c751a2b feat: refactor settings dialog into a page

---
+++ build-elan-ev-opencast-studio-000495-feat-upload-settings-page/precache-manifest.19f3ee60418bc72c556516f5bbfca36b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "69f1de3be268942898d07cd12d33e127",
+    "url": "/build-elan-ev-opencast-studio-000495-feat-upload-settings-page/index.html"
+  },
+  {
+    "revision": "5f0173250514ff94086a",
+    "url": "/build-elan-ev-opencast-studio-000495-feat-upload-settings-page/static/js/2.8c4406b5.chunk.js"
+  },
+  {
+    "revision": "a75501348353fec2f166",
+    "url": "/build-elan-ev-opencast-studio-000495-feat-upload-settings-page/static/js/3.66f0e4ca.chunk.js"
+  },
+  {
+    "revision": "a7821885ab61972a98b2",
+    "url": "/build-elan-ev-opencast-studio-000495-feat-upload-settings-page/static/js/4.45a6fc45.chunk.js"
+  },
+  {
+    "revision": "7500c2964499971dec60",
+    "url": "/build-elan-ev-opencast-studio-000495-feat-upload-settings-page/static/js/main.a4942482.chunk.js"
+  },
+  {
+    "revision": "c4a075a25831223cba8d",
+    "url": "/build-elan-ev-opencast-studio-000495-feat-upload-settings-page/static/js/runtime-main.581c7f45.js"
+  },
+  {
+    "revision": "2f67c1960160f2d005a13ac5e21b9e56",
+    "url": "/build-elan-ev-opencast-studio-000495-feat-upload-settings-page/static/media/iran.2f67c196.png"
+  },
+  {
+    "revision": "6f382b2ddfdd4ea349ac9ca989da123a",
+    "url": "/build-elan-ev-opencast-studio-000495-feat-upload-settings-page/static/media/united-kingdom.6f382b2d.png"
+  }
+]);
---
+++ build-elan-ev-opencast-studio-000495-feat-upload-settings-page/service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/build-elan-ev-opencast-studio-000495-feat-upload-settings-page/precache-manifest.19f3ee60418bc72c556516f5bbfca36b.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/build-elan-ev-opencast-studio-000495-feat-upload-settings-page/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
