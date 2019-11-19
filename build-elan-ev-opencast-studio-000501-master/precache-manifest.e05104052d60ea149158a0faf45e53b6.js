68e19521434ccdcf71a76631fa4ede540ac53cde
Travis CI User <travis@example.org>
2019-11-19 10:38:36 +0000

    Build #501 (Tue Nov 19 10:38:35 UTC 2019) | 0b49038 Merge pull request #221 from elan-ev/dependabot/npm_and_yarn/sentry/browser-5.9.1

---
+++ build-elan-ev-opencast-studio-000501-master/precache-manifest.e05104052d60ea149158a0faf45e53b6.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "45878da319a4396011c3de69b6d68c45",
+    "url": "/build-elan-ev-opencast-studio-000501-master/index.html"
+  },
+  {
+    "revision": "ce7273619919ad01a5f7",
+    "url": "/build-elan-ev-opencast-studio-000501-master/static/js/2.9f488733.chunk.js"
+  },
+  {
+    "revision": "7d278928686a990f6d7d",
+    "url": "/build-elan-ev-opencast-studio-000501-master/static/js/3.ab5eb70f.chunk.js"
+  },
+  {
+    "revision": "7d6635f1c8673a06add6",
+    "url": "/build-elan-ev-opencast-studio-000501-master/static/js/4.f11660c4.chunk.js"
+  },
+  {
+    "revision": "2f9d07798119c20a5a7f",
+    "url": "/build-elan-ev-opencast-studio-000501-master/static/js/main.030c5527.chunk.js"
+  },
+  {
+    "revision": "a08496be9bd51d0ac5eb",
+    "url": "/build-elan-ev-opencast-studio-000501-master/static/js/runtime-main.02e14d61.js"
+  },
+  {
+    "revision": "2f67c1960160f2d005a13ac5e21b9e56",
+    "url": "/build-elan-ev-opencast-studio-000501-master/static/media/iran.2f67c196.png"
+  },
+  {
+    "revision": "6f382b2ddfdd4ea349ac9ca989da123a",
+    "url": "/build-elan-ev-opencast-studio-000501-master/static/media/united-kingdom.6f382b2d.png"
+  }
+]);
