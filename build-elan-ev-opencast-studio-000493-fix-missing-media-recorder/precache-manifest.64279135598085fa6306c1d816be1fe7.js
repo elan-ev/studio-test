7caa850d3a906f66e8d052a913fc3960557d2a61
Travis CI User <travis@example.org>
2019-11-19 07:42:57 +0000

    Build #493 (Tue Nov 19 07:42:57 UTC 2019) | bee64b1 fix: some browsers do not implement MediaRecorder

---
+++ build-elan-ev-opencast-studio-000493-fix-missing-media-recorder/precache-manifest.64279135598085fa6306c1d816be1fe7.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0b4939e778ce52a82dfb4cc43d2c6432",
+    "url": "/build-elan-ev-opencast-studio-000493-fix-missing-media-recorder/index.html"
+  },
+  {
+    "revision": "ce7273619919ad01a5f7",
+    "url": "/build-elan-ev-opencast-studio-000493-fix-missing-media-recorder/static/js/2.9f488733.chunk.js"
+  },
+  {
+    "revision": "a75501348353fec2f166",
+    "url": "/build-elan-ev-opencast-studio-000493-fix-missing-media-recorder/static/js/3.66f0e4ca.chunk.js"
+  },
+  {
+    "revision": "9fc5acd69a2a532c99af",
+    "url": "/build-elan-ev-opencast-studio-000493-fix-missing-media-recorder/static/js/4.28a412d3.chunk.js"
+  },
+  {
+    "revision": "a38284acd06e8909f390",
+    "url": "/build-elan-ev-opencast-studio-000493-fix-missing-media-recorder/static/js/main.3aa5554c.chunk.js"
+  },
+  {
+    "revision": "58b613e720e83913f109",
+    "url": "/build-elan-ev-opencast-studio-000493-fix-missing-media-recorder/static/js/runtime-main.bda51f63.js"
+  },
+  {
+    "revision": "2f67c1960160f2d005a13ac5e21b9e56",
+    "url": "/build-elan-ev-opencast-studio-000493-fix-missing-media-recorder/static/media/iran.2f67c196.png"
+  },
+  {
+    "revision": "6f382b2ddfdd4ea349ac9ca989da123a",
+    "url": "/build-elan-ev-opencast-studio-000493-fix-missing-media-recorder/static/media/united-kingdom.6f382b2d.png"
+  }
+]);
