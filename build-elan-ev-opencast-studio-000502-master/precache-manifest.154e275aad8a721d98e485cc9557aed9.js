5cd6010a78043bd0b57a6e3376a0c550395a2a0e
Travis CI User <travis@example.org>
2019-11-19 10:42:48 +0000

    Build #502 (Tue Nov 19 10:42:48 UTC 2019) | f4d5ceb Merge pull request #222 from elan-ev/fix-missing-media-recorder

---
+++ build-elan-ev-opencast-studio-000502-master/precache-manifest.154e275aad8a721d98e485cc9557aed9.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f0910fc2a50321cb3aec5e2ba7be8924",
+    "url": "/build-elan-ev-opencast-studio-000502-master/index.html"
+  },
+  {
+    "revision": "ce7273619919ad01a5f7",
+    "url": "/build-elan-ev-opencast-studio-000502-master/static/js/2.9f488733.chunk.js"
+  },
+  {
+    "revision": "7d278928686a990f6d7d",
+    "url": "/build-elan-ev-opencast-studio-000502-master/static/js/3.ab5eb70f.chunk.js"
+  },
+  {
+    "revision": "5cff0a6846769279fddf",
+    "url": "/build-elan-ev-opencast-studio-000502-master/static/js/4.00579e69.chunk.js"
+  },
+  {
+    "revision": "08c23f5d36b452b923fa",
+    "url": "/build-elan-ev-opencast-studio-000502-master/static/js/main.d23f843c.chunk.js"
+  },
+  {
+    "revision": "4bcb5c46b61fcae56229",
+    "url": "/build-elan-ev-opencast-studio-000502-master/static/js/runtime-main.7e9d6c1d.js"
+  },
+  {
+    "revision": "2f67c1960160f2d005a13ac5e21b9e56",
+    "url": "/build-elan-ev-opencast-studio-000502-master/static/media/iran.2f67c196.png"
+  },
+  {
+    "revision": "6f382b2ddfdd4ea349ac9ca989da123a",
+    "url": "/build-elan-ev-opencast-studio-000502-master/static/media/united-kingdom.6f382b2d.png"
+  }
+]);
