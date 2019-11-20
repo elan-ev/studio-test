3a2e24d88d163fabaa6f459e48372ece2922a315
Travis CI User <travis@example.org>
2019-11-20 05:01:47 +0000

    Build #517 (Wed Nov 20 05:01:47 UTC 2019) | 86ef4e4 build(deps): bump react-i18next from 11.2.1 to 11.2.2

---
+++ build-elan-ev-opencast-studio-000517-dependabot-npm-and-yarn-react-i18next-11-2-2/precache-manifest.c8db799e3cdb5f01ccd28b0c086ab6c6.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2c306dc516d29617071d7b1eccb2a301",
+    "url": "/build-elan-ev-opencast-studio-000517-dependabot-npm-and-yarn-react-i18next-11-2-2/index.html"
+  },
+  {
+    "revision": "ce7273619919ad01a5f7",
+    "url": "/build-elan-ev-opencast-studio-000517-dependabot-npm-and-yarn-react-i18next-11-2-2/static/js/2.9f488733.chunk.js"
+  },
+  {
+    "revision": "d9a28ee1f4e92ca82a2d",
+    "url": "/build-elan-ev-opencast-studio-000517-dependabot-npm-and-yarn-react-i18next-11-2-2/static/js/3.32f09997.chunk.js"
+  },
+  {
+    "revision": "beab71c77fde018338be",
+    "url": "/build-elan-ev-opencast-studio-000517-dependabot-npm-and-yarn-react-i18next-11-2-2/static/js/4.57f83840.chunk.js"
+  },
+  {
+    "revision": "1cd57ac529d482594595",
+    "url": "/build-elan-ev-opencast-studio-000517-dependabot-npm-and-yarn-react-i18next-11-2-2/static/js/main.59451ee7.chunk.js"
+  },
+  {
+    "revision": "87c269d557ab410a657e",
+    "url": "/build-elan-ev-opencast-studio-000517-dependabot-npm-and-yarn-react-i18next-11-2-2/static/js/runtime-main.316a1ee6.js"
+  },
+  {
+    "revision": "2f67c1960160f2d005a13ac5e21b9e56",
+    "url": "/build-elan-ev-opencast-studio-000517-dependabot-npm-and-yarn-react-i18next-11-2-2/static/media/iran.2f67c196.png"
+  },
+  {
+    "revision": "6f382b2ddfdd4ea349ac9ca989da123a",
+    "url": "/build-elan-ev-opencast-studio-000517-dependabot-npm-and-yarn-react-i18next-11-2-2/static/media/united-kingdom.6f382b2d.png"
+  }
+]);
