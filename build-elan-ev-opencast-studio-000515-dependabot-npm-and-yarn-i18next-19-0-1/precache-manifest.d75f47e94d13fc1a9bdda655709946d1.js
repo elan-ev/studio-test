fadd5ad6a6f8d31fc67add2d3f9adcf6fabd95ab
Travis CI User <travis@example.org>
2019-11-20 05:00:09 +0000

    Build #515 (Wed Nov 20 05:00:09 UTC 2019) | 77ac649 build(deps): bump i18next from 19.0.0 to 19.0.1

---
+++ build-elan-ev-opencast-studio-000515-dependabot-npm-and-yarn-i18next-19-0-1/precache-manifest.d75f47e94d13fc1a9bdda655709946d1.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "793e66448038c20116160b8737930214",
+    "url": "/build-elan-ev-opencast-studio-000515-dependabot-npm-and-yarn-i18next-19-0-1/index.html"
+  },
+  {
+    "revision": "ce7273619919ad01a5f7",
+    "url": "/build-elan-ev-opencast-studio-000515-dependabot-npm-and-yarn-i18next-19-0-1/static/js/2.9f488733.chunk.js"
+  },
+  {
+    "revision": "c9877d91a43446400b0b",
+    "url": "/build-elan-ev-opencast-studio-000515-dependabot-npm-and-yarn-i18next-19-0-1/static/js/3.19a0a7d6.chunk.js"
+  },
+  {
+    "revision": "a15092f281d0d7bd30f0",
+    "url": "/build-elan-ev-opencast-studio-000515-dependabot-npm-and-yarn-i18next-19-0-1/static/js/4.b5c89dbb.chunk.js"
+  },
+  {
+    "revision": "a7068229940285dc4264",
+    "url": "/build-elan-ev-opencast-studio-000515-dependabot-npm-and-yarn-i18next-19-0-1/static/js/main.35e64e79.chunk.js"
+  },
+  {
+    "revision": "d497f4f8bc5133b5e4f1",
+    "url": "/build-elan-ev-opencast-studio-000515-dependabot-npm-and-yarn-i18next-19-0-1/static/js/runtime-main.77dd3a5e.js"
+  },
+  {
+    "revision": "2f67c1960160f2d005a13ac5e21b9e56",
+    "url": "/build-elan-ev-opencast-studio-000515-dependabot-npm-and-yarn-i18next-19-0-1/static/media/iran.2f67c196.png"
+  },
+  {
+    "revision": "6f382b2ddfdd4ea349ac9ca989da123a",
+    "url": "/build-elan-ev-opencast-studio-000515-dependabot-npm-and-yarn-i18next-19-0-1/static/media/united-kingdom.6f382b2d.png"
+  }
+]);
