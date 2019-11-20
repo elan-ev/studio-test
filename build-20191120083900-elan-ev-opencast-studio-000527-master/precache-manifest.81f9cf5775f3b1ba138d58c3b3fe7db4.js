09054c344a164583e1b37798583e1aaff16236c9
Travis CI User <travis@example.org>
2019-11-20 08:39:36 +0000

    Build #527 (Wed Nov 20 08:39:36 UTC 2019) | 2796e0a Remove generated comments from `index.html`

---
+++ build-20191120083900-elan-ev-opencast-studio-000527-master/precache-manifest.81f9cf5775f3b1ba138d58c3b3fe7db4.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fdcb352998725d4872d5962ad5836b42",
+    "url": "/build-20191120083900-elan-ev-opencast-studio-000527-master/index.html"
+  },
+  {
+    "revision": "ce7273619919ad01a5f7",
+    "url": "/build-20191120083900-elan-ev-opencast-studio-000527-master/static/js/2.9f488733.chunk.js"
+  },
+  {
+    "revision": "3d393e18df298ec25b55",
+    "url": "/build-20191120083900-elan-ev-opencast-studio-000527-master/static/js/3.1d5f4c0b.chunk.js"
+  },
+  {
+    "revision": "52d84fc94e338ca22b15",
+    "url": "/build-20191120083900-elan-ev-opencast-studio-000527-master/static/js/4.6714e9c8.chunk.js"
+  },
+  {
+    "revision": "4985a2345f5c66a4177f",
+    "url": "/build-20191120083900-elan-ev-opencast-studio-000527-master/static/js/main.a74a72ba.chunk.js"
+  },
+  {
+    "revision": "8896e55b026fcd6111c6",
+    "url": "/build-20191120083900-elan-ev-opencast-studio-000527-master/static/js/runtime-main.3d4c608b.js"
+  },
+  {
+    "revision": "2f67c1960160f2d005a13ac5e21b9e56",
+    "url": "/build-20191120083900-elan-ev-opencast-studio-000527-master/static/media/iran.2f67c196.png"
+  },
+  {
+    "revision": "6f382b2ddfdd4ea349ac9ca989da123a",
+    "url": "/build-20191120083900-elan-ev-opencast-studio-000527-master/static/media/united-kingdom.6f382b2d.png"
+  }
+]);
---
+++ build-20191120083900-elan-ev-opencast-studio-000527-master/service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/build-20191120083900-elan-ev-opencast-studio-000527-master/precache-manifest.81f9cf5775f3b1ba138d58c3b3fe7db4.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/build-20191120083900-elan-ev-opencast-studio-000527-master/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
