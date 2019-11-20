eef85b9cb7d305cc0b8946f1e312c1a2f3c5e944
Travis CI User <travis@example.org>
2019-11-20 08:07:15 +0000

    Build #521 (Wed Nov 20 08:07:15 UTC 2019) | 53d700d build(deps): bump react-i18next from 11.2.1 to 11.2.2

---
+++ build-elan-ev-opencast-studio-000521-master/precache-manifest.733afec62c29809ce11b9a1d55d0e858.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b93f695124fd1dc290df56e3a39f0b23",
+    "url": "/build-elan-ev-opencast-studio-000521-master/index.html"
+  },
+  {
+    "revision": "ce7273619919ad01a5f7",
+    "url": "/build-elan-ev-opencast-studio-000521-master/static/js/2.9f488733.chunk.js"
+  },
+  {
+    "revision": "d9a28ee1f4e92ca82a2d",
+    "url": "/build-elan-ev-opencast-studio-000521-master/static/js/3.32f09997.chunk.js"
+  },
+  {
+    "revision": "c8d1f78b40642802a449",
+    "url": "/build-elan-ev-opencast-studio-000521-master/static/js/4.9d3955e4.chunk.js"
+  },
+  {
+    "revision": "ac47be126445e3f794c4",
+    "url": "/build-elan-ev-opencast-studio-000521-master/static/js/main.4283dc52.chunk.js"
+  },
+  {
+    "revision": "cfcb3f126e77bf735b14",
+    "url": "/build-elan-ev-opencast-studio-000521-master/static/js/runtime-main.7dab86d5.js"
+  },
+  {
+    "revision": "2f67c1960160f2d005a13ac5e21b9e56",
+    "url": "/build-elan-ev-opencast-studio-000521-master/static/media/iran.2f67c196.png"
+  },
+  {
+    "revision": "6f382b2ddfdd4ea349ac9ca989da123a",
+    "url": "/build-elan-ev-opencast-studio-000521-master/static/media/united-kingdom.6f382b2d.png"
+  }
+]);
