1770e95f201434951a00b9aa7f1fd94dd8b50fae
Travis CI User <travis@example.org>
2019-11-20 08:22:39 +0000

    Build #526 (Wed Nov 20 08:22:39 UTC 2019) | c0239d9 fix: apply `npm audit fix` to update handlebars version

---
+++ build-20191120082203-elan-ev-opencast-studio-000526-master/precache-manifest.60585a8c3657f9c167daeb34d1e890f1.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e4b19762122ed1973d4f4f8f8f970fec",
+    "url": "/build-20191120082203-elan-ev-opencast-studio-000526-master/index.html"
+  },
+  {
+    "revision": "ce7273619919ad01a5f7",
+    "url": "/build-20191120082203-elan-ev-opencast-studio-000526-master/static/js/2.9f488733.chunk.js"
+  },
+  {
+    "revision": "3d393e18df298ec25b55",
+    "url": "/build-20191120082203-elan-ev-opencast-studio-000526-master/static/js/3.1d5f4c0b.chunk.js"
+  },
+  {
+    "revision": "cdc495690a2cee052469",
+    "url": "/build-20191120082203-elan-ev-opencast-studio-000526-master/static/js/4.a8618bab.chunk.js"
+  },
+  {
+    "revision": "f427f08dd3018b9172d1",
+    "url": "/build-20191120082203-elan-ev-opencast-studio-000526-master/static/js/main.4723fb5a.chunk.js"
+  },
+  {
+    "revision": "32daae860a2b8d624f9a",
+    "url": "/build-20191120082203-elan-ev-opencast-studio-000526-master/static/js/runtime-main.d435f816.js"
+  },
+  {
+    "revision": "2f67c1960160f2d005a13ac5e21b9e56",
+    "url": "/build-20191120082203-elan-ev-opencast-studio-000526-master/static/media/iran.2f67c196.png"
+  },
+  {
+    "revision": "6f382b2ddfdd4ea349ac9ca989da123a",
+    "url": "/build-20191120082203-elan-ev-opencast-studio-000526-master/static/media/united-kingdom.6f382b2d.png"
+  }
+]);
