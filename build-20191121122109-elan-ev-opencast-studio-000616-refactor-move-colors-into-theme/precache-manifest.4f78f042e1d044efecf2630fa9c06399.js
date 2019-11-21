41fa057ddbe00a1d49f941cb6ea7b54897bdd18f
Travis CI User <travis@example.org>
2019-11-21 12:21:49 +0000

    Build #616 (Thu Nov 21 12:21:48 UTC 2019) | dd88502 refactor: move colors into theme definition

---
+++ build-20191121122109-elan-ev-opencast-studio-000616-refactor-move-colors-into-theme/precache-manifest.4f78f042e1d044efecf2630fa9c06399.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8edc60863a5ba30bd0d1f4f1c0cb2c64",
+    "url": "/build-20191121122109-elan-ev-opencast-studio-000616-refactor-move-colors-into-theme/index.html"
+  },
+  {
+    "revision": "6e07e1abab5bbad4a5f9",
+    "url": "/build-20191121122109-elan-ev-opencast-studio-000616-refactor-move-colors-into-theme/static/js/2.9d9348f7.chunk.js"
+  },
+  {
+    "revision": "08ea32e3ccd4f7f1b169",
+    "url": "/build-20191121122109-elan-ev-opencast-studio-000616-refactor-move-colors-into-theme/static/js/3.0c6ca0cd.chunk.js"
+  },
+  {
+    "revision": "1d84c40061505548ce8c",
+    "url": "/build-20191121122109-elan-ev-opencast-studio-000616-refactor-move-colors-into-theme/static/js/4.506a3f9c.chunk.js"
+  },
+  {
+    "revision": "c800c4280a1c6677d6ae",
+    "url": "/build-20191121122109-elan-ev-opencast-studio-000616-refactor-move-colors-into-theme/static/js/main.14a3e946.chunk.js"
+  },
+  {
+    "revision": "553b5390000a43669704",
+    "url": "/build-20191121122109-elan-ev-opencast-studio-000616-refactor-move-colors-into-theme/static/js/runtime-main.e3232a84.js"
+  },
+  {
+    "revision": "2f67c1960160f2d005a13ac5e21b9e56",
+    "url": "/build-20191121122109-elan-ev-opencast-studio-000616-refactor-move-colors-into-theme/static/media/iran.2f67c196.png"
+  },
+  {
+    "revision": "6f382b2ddfdd4ea349ac9ca989da123a",
+    "url": "/build-20191121122109-elan-ev-opencast-studio-000616-refactor-move-colors-into-theme/static/media/united-kingdom.6f382b2d.png"
+  }
+]);
