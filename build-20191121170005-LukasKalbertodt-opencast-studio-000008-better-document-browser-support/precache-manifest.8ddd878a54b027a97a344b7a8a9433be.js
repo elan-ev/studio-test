665f35b553e323d9de828bd1fe1ffb149b0583ea
Travis CI User <travis@example.org>
2019-11-21 17:00:42 +0000

    Build #8 (Thu Nov 21 17:00:42 UTC 2019) | b369ab7 Add "supported browsers" table to README

---
+++ build-20191121170005-LukasKalbertodt-opencast-studio-000008-better-document-browser-support/precache-manifest.8ddd878a54b027a97a344b7a8a9433be.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "97d14675da44ceac8e230df1b67058cd",
+    "url": "/build-20191121170005-LukasKalbertodt-opencast-studio-000008-better-document-browser-support/index.html"
+  },
+  {
+    "revision": "b04229fd502e2093e89a",
+    "url": "/build-20191121170005-LukasKalbertodt-opencast-studio-000008-better-document-browser-support/static/js/2.d52f3280.chunk.js"
+  },
+  {
+    "revision": "d3ce50699748d438a068",
+    "url": "/build-20191121170005-LukasKalbertodt-opencast-studio-000008-better-document-browser-support/static/js/3.6022d81e.chunk.js"
+  },
+  {
+    "revision": "1a6838ea2f1a44c48c9f",
+    "url": "/build-20191121170005-LukasKalbertodt-opencast-studio-000008-better-document-browser-support/static/js/4.a18e51e5.chunk.js"
+  },
+  {
+    "revision": "8cee54a024093da52b9e",
+    "url": "/build-20191121170005-LukasKalbertodt-opencast-studio-000008-better-document-browser-support/static/js/main.926216c9.chunk.js"
+  },
+  {
+    "revision": "d006b85c3b92ff3c7b4f",
+    "url": "/build-20191121170005-LukasKalbertodt-opencast-studio-000008-better-document-browser-support/static/js/runtime-main.9d8868cf.js"
+  },
+  {
+    "revision": "2f67c1960160f2d005a13ac5e21b9e56",
+    "url": "/build-20191121170005-LukasKalbertodt-opencast-studio-000008-better-document-browser-support/static/media/iran.2f67c196.png"
+  },
+  {
+    "revision": "6f382b2ddfdd4ea349ac9ca989da123a",
+    "url": "/build-20191121170005-LukasKalbertodt-opencast-studio-000008-better-document-browser-support/static/media/united-kingdom.6f382b2d.png"
+  }
+]);
---
+++ build-20191121170005-LukasKalbertodt-opencast-studio-000008-better-document-browser-support/service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/build-20191121170005-LukasKalbertodt-opencast-studio-000008-better-document-browser-support/precache-manifest.8ddd878a54b027a97a344b7a8a9433be.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/build-20191121170005-LukasKalbertodt-opencast-studio-000008-better-document-browser-support/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
