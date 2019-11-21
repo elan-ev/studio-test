4a6475a69bc1d45f600f09420acf5a588e6a3dac
Travis CI User <travis@example.org>
2019-11-21 08:02:23 +0000

    Build #560 (Thu Nov 21 08:02:23 UTC 2019) | f1b1fa4 feat: refactor settings dialog into a page

---
+++ build-20191121080147-elan-ev-opencast-studio-000560-feat-upload-settings-page/precache-manifest.419d4cb34a8344b2bca0dcde652f7815.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d957e33656b703c89710ab3b692c0013",
+    "url": "/build-20191121080147-elan-ev-opencast-studio-000560-feat-upload-settings-page/index.html"
+  },
+  {
+    "revision": "09306d74ce9bda638eff",
+    "url": "/build-20191121080147-elan-ev-opencast-studio-000560-feat-upload-settings-page/static/js/2.0d1de926.chunk.js"
+  },
+  {
+    "revision": "3d393e18df298ec25b55",
+    "url": "/build-20191121080147-elan-ev-opencast-studio-000560-feat-upload-settings-page/static/js/3.1d5f4c0b.chunk.js"
+  },
+  {
+    "revision": "4a7d18a164b537aacdc8",
+    "url": "/build-20191121080147-elan-ev-opencast-studio-000560-feat-upload-settings-page/static/js/4.fcadb457.chunk.js"
+  },
+  {
+    "revision": "4c26bb1e40a02b8942f9",
+    "url": "/build-20191121080147-elan-ev-opencast-studio-000560-feat-upload-settings-page/static/js/main.f69e658f.chunk.js"
+  },
+  {
+    "revision": "66e45adaa0bb001583f0",
+    "url": "/build-20191121080147-elan-ev-opencast-studio-000560-feat-upload-settings-page/static/js/runtime-main.52cd319c.js"
+  },
+  {
+    "revision": "2f67c1960160f2d005a13ac5e21b9e56",
+    "url": "/build-20191121080147-elan-ev-opencast-studio-000560-feat-upload-settings-page/static/media/iran.2f67c196.png"
+  },
+  {
+    "revision": "6f382b2ddfdd4ea349ac9ca989da123a",
+    "url": "/build-20191121080147-elan-ev-opencast-studio-000560-feat-upload-settings-page/static/media/united-kingdom.6f382b2d.png"
+  }
+]);
