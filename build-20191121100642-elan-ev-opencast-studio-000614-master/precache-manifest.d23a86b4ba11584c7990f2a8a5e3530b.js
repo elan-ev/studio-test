0d7254391b66ecfa6eda02c38c9621a2a8baadaa
Travis CI User <travis@example.org>
2019-11-21 10:07:24 +0000

    Build #614 (Thu Nov 21 10:07:24 UTC 2019) | 3e1dc03 refactor: move from @reach/router to react-router

---
+++ build-20191121100642-elan-ev-opencast-studio-000614-master/precache-manifest.d23a86b4ba11584c7990f2a8a5e3530b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e8f12878b10ff78950749b45c7f91de1",
+    "url": "/build-20191121100642-elan-ev-opencast-studio-000614-master/index.html"
+  },
+  {
+    "revision": "65c0188a0df00c03971f",
+    "url": "/build-20191121100642-elan-ev-opencast-studio-000614-master/static/js/2.1144fee2.chunk.js"
+  },
+  {
+    "revision": "d3ce50699748d438a068",
+    "url": "/build-20191121100642-elan-ev-opencast-studio-000614-master/static/js/3.6022d81e.chunk.js"
+  },
+  {
+    "revision": "f7ef9e645b766b98f6e0",
+    "url": "/build-20191121100642-elan-ev-opencast-studio-000614-master/static/js/4.14ac9342.chunk.js"
+  },
+  {
+    "revision": "a9aa536701748b45c42b",
+    "url": "/build-20191121100642-elan-ev-opencast-studio-000614-master/static/js/main.853ccb46.chunk.js"
+  },
+  {
+    "revision": "2db03c412ddc9018c533",
+    "url": "/build-20191121100642-elan-ev-opencast-studio-000614-master/static/js/runtime-main.26edea26.js"
+  },
+  {
+    "revision": "2f67c1960160f2d005a13ac5e21b9e56",
+    "url": "/build-20191121100642-elan-ev-opencast-studio-000614-master/static/media/iran.2f67c196.png"
+  },
+  {
+    "revision": "6f382b2ddfdd4ea349ac9ca989da123a",
+    "url": "/build-20191121100642-elan-ev-opencast-studio-000614-master/static/media/united-kingdom.6f382b2d.png"
+  }
+]);
