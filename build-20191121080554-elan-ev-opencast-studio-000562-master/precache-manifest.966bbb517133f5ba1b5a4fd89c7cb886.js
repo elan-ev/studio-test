f2ca6c8b1a4aab95f7246cdc2f4b84939981bf78
Travis CI User <travis@example.org>
2019-11-21 08:06:31 +0000

    Build #562 (Thu Nov 21 08:06:31 UTC 2019) | ff20acf feat: refactor settings dialog into a page

---
+++ build-20191121080554-elan-ev-opencast-studio-000562-master/precache-manifest.966bbb517133f5ba1b5a4fd89c7cb886.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f3f3435b3fcea2b6dc36b6ea7d33342a",
+    "url": "/build-20191121080554-elan-ev-opencast-studio-000562-master/index.html"
+  },
+  {
+    "revision": "09306d74ce9bda638eff",
+    "url": "/build-20191121080554-elan-ev-opencast-studio-000562-master/static/js/2.0d1de926.chunk.js"
+  },
+  {
+    "revision": "3d393e18df298ec25b55",
+    "url": "/build-20191121080554-elan-ev-opencast-studio-000562-master/static/js/3.1d5f4c0b.chunk.js"
+  },
+  {
+    "revision": "921754a7b8381901a384",
+    "url": "/build-20191121080554-elan-ev-opencast-studio-000562-master/static/js/4.91d2ef30.chunk.js"
+  },
+  {
+    "revision": "5419298f048935fcc877",
+    "url": "/build-20191121080554-elan-ev-opencast-studio-000562-master/static/js/main.b4aa7661.chunk.js"
+  },
+  {
+    "revision": "20bafe003324796154b1",
+    "url": "/build-20191121080554-elan-ev-opencast-studio-000562-master/static/js/runtime-main.b79dacd9.js"
+  },
+  {
+    "revision": "2f67c1960160f2d005a13ac5e21b9e56",
+    "url": "/build-20191121080554-elan-ev-opencast-studio-000562-master/static/media/iran.2f67c196.png"
+  },
+  {
+    "revision": "6f382b2ddfdd4ea349ac9ca989da123a",
+    "url": "/build-20191121080554-elan-ev-opencast-studio-000562-master/static/media/united-kingdom.6f382b2d.png"
+  }
+]);
