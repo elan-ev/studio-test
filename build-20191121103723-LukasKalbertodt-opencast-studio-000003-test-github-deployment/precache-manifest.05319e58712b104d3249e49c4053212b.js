8c10faad2ad30043a16aa245161f8591d63fb213
Travis CI User <travis@example.org>
2019-11-21 10:38:03 +0000

    Build #3 (Thu Nov 21 10:38:03 UTC 2019) | b33adc0 Test

---
+++ build-20191121103723-LukasKalbertodt-opencast-studio-000003-test-github-deployment/precache-manifest.05319e58712b104d3249e49c4053212b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8fa24798a9e92a2b8a1be1f6581c9bdd",
+    "url": "/build-20191121103723-LukasKalbertodt-opencast-studio-000003-test-github-deployment/index.html"
+  },
+  {
+    "revision": "bccb3607a80e90c5e720",
+    "url": "/build-20191121103723-LukasKalbertodt-opencast-studio-000003-test-github-deployment/static/js/2.993ed798.chunk.js"
+  },
+  {
+    "revision": "3d393e18df298ec25b55",
+    "url": "/build-20191121103723-LukasKalbertodt-opencast-studio-000003-test-github-deployment/static/js/3.1d5f4c0b.chunk.js"
+  },
+  {
+    "revision": "99fb66c5edade59634d1",
+    "url": "/build-20191121103723-LukasKalbertodt-opencast-studio-000003-test-github-deployment/static/js/4.5cb1d2d2.chunk.js"
+  },
+  {
+    "revision": "e931f4cfc47c5a3f3afe",
+    "url": "/build-20191121103723-LukasKalbertodt-opencast-studio-000003-test-github-deployment/static/js/main.9e52e54b.chunk.js"
+  },
+  {
+    "revision": "7b40081c02878e57b205",
+    "url": "/build-20191121103723-LukasKalbertodt-opencast-studio-000003-test-github-deployment/static/js/runtime-main.26f7bf2e.js"
+  },
+  {
+    "revision": "2f67c1960160f2d005a13ac5e21b9e56",
+    "url": "/build-20191121103723-LukasKalbertodt-opencast-studio-000003-test-github-deployment/static/media/iran.2f67c196.png"
+  },
+  {
+    "revision": "6f382b2ddfdd4ea349ac9ca989da123a",
+    "url": "/build-20191121103723-LukasKalbertodt-opencast-studio-000003-test-github-deployment/static/media/united-kingdom.6f382b2d.png"
+  }
+]);
---
+++ build-20191121103723-LukasKalbertodt-opencast-studio-000003-test-github-deployment/service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/build-20191121103723-LukasKalbertodt-opencast-studio-000003-test-github-deployment/precache-manifest.05319e58712b104d3249e49c4053212b.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/build-20191121103723-LukasKalbertodt-opencast-studio-000003-test-github-deployment/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
