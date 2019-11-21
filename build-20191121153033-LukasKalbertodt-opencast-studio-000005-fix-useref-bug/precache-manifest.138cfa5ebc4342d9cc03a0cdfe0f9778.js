04db4ff7bc7510e98955124ee45dfe46ac08a85d
Travis CI User <travis@example.org>
2019-11-21 15:31:17 +0000

    Build #5 (Thu Nov 21 15:31:16 UTC 2019) | 9ff45c5 Fix bug when using `useRef`

---
+++ build-20191121153033-LukasKalbertodt-opencast-studio-000005-fix-useref-bug/precache-manifest.138cfa5ebc4342d9cc03a0cdfe0f9778.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0314e9e7fc297e0e2c3bb0b4281e2fc5",
+    "url": "/build-20191121153033-LukasKalbertodt-opencast-studio-000005-fix-useref-bug/index.html"
+  },
+  {
+    "revision": "b04229fd502e2093e89a",
+    "url": "/build-20191121153033-LukasKalbertodt-opencast-studio-000005-fix-useref-bug/static/js/2.d52f3280.chunk.js"
+  },
+  {
+    "revision": "d3ce50699748d438a068",
+    "url": "/build-20191121153033-LukasKalbertodt-opencast-studio-000005-fix-useref-bug/static/js/3.6022d81e.chunk.js"
+  },
+  {
+    "revision": "912d1e2de4f8d8eacdc6",
+    "url": "/build-20191121153033-LukasKalbertodt-opencast-studio-000005-fix-useref-bug/static/js/4.5601856d.chunk.js"
+  },
+  {
+    "revision": "c5c926ec67abfb554eb5",
+    "url": "/build-20191121153033-LukasKalbertodt-opencast-studio-000005-fix-useref-bug/static/js/main.52471246.chunk.js"
+  },
+  {
+    "revision": "035f4f5c6db6da5a62cf",
+    "url": "/build-20191121153033-LukasKalbertodt-opencast-studio-000005-fix-useref-bug/static/js/runtime-main.1d6b490f.js"
+  },
+  {
+    "revision": "2f67c1960160f2d005a13ac5e21b9e56",
+    "url": "/build-20191121153033-LukasKalbertodt-opencast-studio-000005-fix-useref-bug/static/media/iran.2f67c196.png"
+  },
+  {
+    "revision": "6f382b2ddfdd4ea349ac9ca989da123a",
+    "url": "/build-20191121153033-LukasKalbertodt-opencast-studio-000005-fix-useref-bug/static/media/united-kingdom.6f382b2d.png"
+  }
+]);
