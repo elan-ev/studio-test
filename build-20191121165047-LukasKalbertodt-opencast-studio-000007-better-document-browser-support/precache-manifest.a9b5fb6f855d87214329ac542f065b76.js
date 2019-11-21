45e84249e2f950bec6c777a0d0d53ede033be1df
Travis CI User <travis@example.org>
2019-11-21 16:51:25 +0000

    Build #7 (Thu Nov 21 16:51:25 UTC 2019) | 96d2933 Add "supported browsers" table to README

---
+++ build-20191121165047-LukasKalbertodt-opencast-studio-000007-better-document-browser-support/precache-manifest.a9b5fb6f855d87214329ac542f065b76.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a9717f706d90b34ea7c359515da9c4b0",
+    "url": "/build-20191121165047-LukasKalbertodt-opencast-studio-000007-better-document-browser-support/index.html"
+  },
+  {
+    "revision": "b04229fd502e2093e89a",
+    "url": "/build-20191121165047-LukasKalbertodt-opencast-studio-000007-better-document-browser-support/static/js/2.d52f3280.chunk.js"
+  },
+  {
+    "revision": "d3ce50699748d438a068",
+    "url": "/build-20191121165047-LukasKalbertodt-opencast-studio-000007-better-document-browser-support/static/js/3.6022d81e.chunk.js"
+  },
+  {
+    "revision": "c0a82210c7b26c4e7f43",
+    "url": "/build-20191121165047-LukasKalbertodt-opencast-studio-000007-better-document-browser-support/static/js/4.36ab1528.chunk.js"
+  },
+  {
+    "revision": "c97d908377b3c2de99f5",
+    "url": "/build-20191121165047-LukasKalbertodt-opencast-studio-000007-better-document-browser-support/static/js/main.21a341f2.chunk.js"
+  },
+  {
+    "revision": "cf244de025dc0b11eada",
+    "url": "/build-20191121165047-LukasKalbertodt-opencast-studio-000007-better-document-browser-support/static/js/runtime-main.6f7cc6ee.js"
+  },
+  {
+    "revision": "2f67c1960160f2d005a13ac5e21b9e56",
+    "url": "/build-20191121165047-LukasKalbertodt-opencast-studio-000007-better-document-browser-support/static/media/iran.2f67c196.png"
+  },
+  {
+    "revision": "6f382b2ddfdd4ea349ac9ca989da123a",
+    "url": "/build-20191121165047-LukasKalbertodt-opencast-studio-000007-better-document-browser-support/static/media/united-kingdom.6f382b2d.png"
+  }
+]);
