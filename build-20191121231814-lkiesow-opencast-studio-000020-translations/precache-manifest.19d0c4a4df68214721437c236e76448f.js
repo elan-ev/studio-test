c074e823852912c6bd480b4f5f3035e479a05dfc
Travis CI User <travis@example.org>
2019-11-21 23:18:51 +0000

    Build #20 (Thu Nov 21 23:18:51 UTC 2019) | 3f3023f Fix Source Translations

---
+++ build-20191121231814-lkiesow-opencast-studio-000020-translations/precache-manifest.19d0c4a4df68214721437c236e76448f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d83b8ec37a5ba95366d269fe5163b582",
+    "url": "/build-20191121231814-lkiesow-opencast-studio-000020-translations/index.html"
+  },
+  {
+    "revision": "92db024256a6d59acf20",
+    "url": "/build-20191121231814-lkiesow-opencast-studio-000020-translations/static/js/2.77f775c1.chunk.js"
+  },
+  {
+    "revision": "d3ce50699748d438a068",
+    "url": "/build-20191121231814-lkiesow-opencast-studio-000020-translations/static/js/3.6022d81e.chunk.js"
+  },
+  {
+    "revision": "f589ab3287e63f34ad54",
+    "url": "/build-20191121231814-lkiesow-opencast-studio-000020-translations/static/js/4.a653d223.chunk.js"
+  },
+  {
+    "revision": "cc359baec0207a780b65",
+    "url": "/build-20191121231814-lkiesow-opencast-studio-000020-translations/static/js/main.25426328.chunk.js"
+  },
+  {
+    "revision": "31915714d2805bd607d9",
+    "url": "/build-20191121231814-lkiesow-opencast-studio-000020-translations/static/js/runtime-main.acc720a8.js"
+  },
+  {
+    "revision": "2f67c1960160f2d005a13ac5e21b9e56",
+    "url": "/build-20191121231814-lkiesow-opencast-studio-000020-translations/static/media/iran.2f67c196.png"
+  },
+  {
+    "revision": "6f382b2ddfdd4ea349ac9ca989da123a",
+    "url": "/build-20191121231814-lkiesow-opencast-studio-000020-translations/static/media/united-kingdom.6f382b2d.png"
+  }
+]);
