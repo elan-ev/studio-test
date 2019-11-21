ae48bb40d986108dfd3689e5d790daa4ff75e62d
Travis CI User <travis@example.org>
2019-11-21 23:27:45 +0000

    Build #22 (Thu Nov 21 23:27:44 UTC 2019) | de68497 Include Build Information

---
+++ build-20191121232706-lkiesow-opencast-studio-000022-build-number/precache-manifest.d5a9fd6435b56b3c9e0d5b223bc671e7.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ca3810bdea85d915334e0f41f8eaceac",
+    "url": "/build-20191121232706-lkiesow-opencast-studio-000022-build-number/index.html"
+  },
+  {
+    "revision": "92db024256a6d59acf20",
+    "url": "/build-20191121232706-lkiesow-opencast-studio-000022-build-number/static/js/2.77f775c1.chunk.js"
+  },
+  {
+    "revision": "d3ce50699748d438a068",
+    "url": "/build-20191121232706-lkiesow-opencast-studio-000022-build-number/static/js/3.6022d81e.chunk.js"
+  },
+  {
+    "revision": "8a35c6c80b3a0cf621cd",
+    "url": "/build-20191121232706-lkiesow-opencast-studio-000022-build-number/static/js/4.3f2b1d7e.chunk.js"
+  },
+  {
+    "revision": "2e28aad715511b1d8148",
+    "url": "/build-20191121232706-lkiesow-opencast-studio-000022-build-number/static/js/main.ad81d1e8.chunk.js"
+  },
+  {
+    "revision": "f5edd92d4f9b06d5b548",
+    "url": "/build-20191121232706-lkiesow-opencast-studio-000022-build-number/static/js/runtime-main.4f9a4012.js"
+  },
+  {
+    "revision": "2f67c1960160f2d005a13ac5e21b9e56",
+    "url": "/build-20191121232706-lkiesow-opencast-studio-000022-build-number/static/media/iran.2f67c196.png"
+  },
+  {
+    "revision": "6f382b2ddfdd4ea349ac9ca989da123a",
+    "url": "/build-20191121232706-lkiesow-opencast-studio-000022-build-number/static/media/united-kingdom.6f382b2d.png"
+  }
+]);
