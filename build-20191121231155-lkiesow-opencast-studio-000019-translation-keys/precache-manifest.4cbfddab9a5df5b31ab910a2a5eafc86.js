8b62b9a919f51ba2a21295d8ffadb8fa21ed9586
Travis CI User <travis@example.org>
2019-11-21 23:12:32 +0000

    Build #19 (Thu Nov 21 23:12:32 UTC 2019) | 434f46e Fix Translation Keys

---
+++ build-20191121231155-lkiesow-opencast-studio-000019-translation-keys/precache-manifest.4cbfddab9a5df5b31ab910a2a5eafc86.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6cd37b21403b4adae0c14d36090fca91",
+    "url": "/build-20191121231155-lkiesow-opencast-studio-000019-translation-keys/index.html"
+  },
+  {
+    "revision": "92db024256a6d59acf20",
+    "url": "/build-20191121231155-lkiesow-opencast-studio-000019-translation-keys/static/js/2.77f775c1.chunk.js"
+  },
+  {
+    "revision": "d3ce50699748d438a068",
+    "url": "/build-20191121231155-lkiesow-opencast-studio-000019-translation-keys/static/js/3.6022d81e.chunk.js"
+  },
+  {
+    "revision": "1716783775264b25031b",
+    "url": "/build-20191121231155-lkiesow-opencast-studio-000019-translation-keys/static/js/4.2c218c7b.chunk.js"
+  },
+  {
+    "revision": "d74e998fc31208a5014f",
+    "url": "/build-20191121231155-lkiesow-opencast-studio-000019-translation-keys/static/js/main.ecc91754.chunk.js"
+  },
+  {
+    "revision": "e046985423fef1b5e013",
+    "url": "/build-20191121231155-lkiesow-opencast-studio-000019-translation-keys/static/js/runtime-main.3bc0ab0e.js"
+  },
+  {
+    "revision": "2f67c1960160f2d005a13ac5e21b9e56",
+    "url": "/build-20191121231155-lkiesow-opencast-studio-000019-translation-keys/static/media/iran.2f67c196.png"
+  },
+  {
+    "revision": "6f382b2ddfdd4ea349ac9ca989da123a",
+    "url": "/build-20191121231155-lkiesow-opencast-studio-000019-translation-keys/static/media/united-kingdom.6f382b2d.png"
+  }
+]);
---
+++ build-20191121231155-lkiesow-opencast-studio-000019-translation-keys/service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/build-20191121231155-lkiesow-opencast-studio-000019-translation-keys/precache-manifest.4cbfddab9a5df5b31ab910a2a5eafc86.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/build-20191121231155-lkiesow-opencast-studio-000019-translation-keys/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
