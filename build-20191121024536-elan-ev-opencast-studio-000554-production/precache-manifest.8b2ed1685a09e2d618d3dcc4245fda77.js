75b17e3c0e50d8316422693769411818fbd2bd85
Travis CI User <travis@example.org>
2019-11-21 02:46:11 +0000

    Build #554 (Thu Nov 21 02:46:11 UTC 2019) | 8bda355 Merge pull request #228 from LukasKalbertodt/move-legal-notices

---
+++ build-20191121024536-elan-ev-opencast-studio-000554-production/precache-manifest.8b2ed1685a09e2d618d3dcc4245fda77.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d84315f7da9ed03c73f8ef78bce5553a",
+    "url": "/build-20191121024536-elan-ev-opencast-studio-000554-production/index.html"
+  },
+  {
+    "revision": "8f399b9f324ae944c9df",
+    "url": "/build-20191121024536-elan-ev-opencast-studio-000554-production/static/js/2.89b21343.chunk.js"
+  },
+  {
+    "revision": "3d393e18df298ec25b55",
+    "url": "/build-20191121024536-elan-ev-opencast-studio-000554-production/static/js/3.1d5f4c0b.chunk.js"
+  },
+  {
+    "revision": "bb43d653d7e8f19c225f",
+    "url": "/build-20191121024536-elan-ev-opencast-studio-000554-production/static/js/4.6e673e57.chunk.js"
+  },
+  {
+    "revision": "48d324b8e79aa8f6e8af",
+    "url": "/build-20191121024536-elan-ev-opencast-studio-000554-production/static/js/main.74bf3d77.chunk.js"
+  },
+  {
+    "revision": "f23fda46d5dde8e005c3",
+    "url": "/build-20191121024536-elan-ev-opencast-studio-000554-production/static/js/runtime-main.305b3a2b.js"
+  },
+  {
+    "revision": "2f67c1960160f2d005a13ac5e21b9e56",
+    "url": "/build-20191121024536-elan-ev-opencast-studio-000554-production/static/media/iran.2f67c196.png"
+  },
+  {
+    "revision": "6f382b2ddfdd4ea349ac9ca989da123a",
+    "url": "/build-20191121024536-elan-ev-opencast-studio-000554-production/static/media/united-kingdom.6f382b2d.png"
+  }
+]);
