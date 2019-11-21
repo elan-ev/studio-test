b7eab6e556985d8e1c2f14da08122e7ab85ecfe3
Travis CI User <travis@example.org>
2019-11-21 16:09:13 +0000

    Build #6 (Thu Nov 21 16:09:13 UTC 2019) | 0cecc3b Fix bug when using `useRef` to set `video.srcObject`

---
+++ build-20191121160836-LukasKalbertodt-opencast-studio-000006-fix-useref-bug/precache-manifest.22f46c141020900a135cdeab6ae921bd.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c0cdc231adbe2776768c1063bd4ef7e6",
+    "url": "/build-20191121160836-LukasKalbertodt-opencast-studio-000006-fix-useref-bug/index.html"
+  },
+  {
+    "revision": "b04229fd502e2093e89a",
+    "url": "/build-20191121160836-LukasKalbertodt-opencast-studio-000006-fix-useref-bug/static/js/2.d52f3280.chunk.js"
+  },
+  {
+    "revision": "d3ce50699748d438a068",
+    "url": "/build-20191121160836-LukasKalbertodt-opencast-studio-000006-fix-useref-bug/static/js/3.6022d81e.chunk.js"
+  },
+  {
+    "revision": "d5c314546189051e6689",
+    "url": "/build-20191121160836-LukasKalbertodt-opencast-studio-000006-fix-useref-bug/static/js/4.546e4a98.chunk.js"
+  },
+  {
+    "revision": "fff14d98c706b8796946",
+    "url": "/build-20191121160836-LukasKalbertodt-opencast-studio-000006-fix-useref-bug/static/js/main.0136d312.chunk.js"
+  },
+  {
+    "revision": "ee0ade647ff5826e5d22",
+    "url": "/build-20191121160836-LukasKalbertodt-opencast-studio-000006-fix-useref-bug/static/js/runtime-main.def71f6b.js"
+  },
+  {
+    "revision": "2f67c1960160f2d005a13ac5e21b9e56",
+    "url": "/build-20191121160836-LukasKalbertodt-opencast-studio-000006-fix-useref-bug/static/media/iran.2f67c196.png"
+  },
+  {
+    "revision": "6f382b2ddfdd4ea349ac9ca989da123a",
+    "url": "/build-20191121160836-LukasKalbertodt-opencast-studio-000006-fix-useref-bug/static/media/united-kingdom.6f382b2d.png"
+  }
+]);
